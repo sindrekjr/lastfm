7c825325c4864a4ffbfe2aaa54c4345367dedc2a
Sindre S. Kjær <sindre@sindrekjr.no>
2021-08-20 04:07:48 +0200

    chore(api): add some tests

---
+++ src/api/client.spec.ts
@@ -0,0 +1,15 @@
+import { ApiClient } from './client.class';
+
+describe('ApiClient', () => {
+  it('should not throw', () => expect(() => new ApiClient('')).not.toThrow());
+
+  it('should store given api key', () => {
+    const client = new ApiClient('testApiKey');
+    expect(Object.values(client)).toContain('testApiKey');
+  });
+
+  it('should store given shared secret', () => {
+    const client = new ApiClient('', 'testSharedSecret');
+    expect(Object.values(client)).toContain('testSharedSecret');
+  })
+});
